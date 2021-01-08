7a3b2ce9c295628b08a8591579b13254018a1715
user <sairysss@gmail.com>
2021-01-08 19:27:55 +0100

    docs: explaining type compatibility of value objects

--- src/modules/user/domain/value-objects/email.value-object.ts
+++ src/modules/user/domain/value-objects/email.value-object.ts
@@ -10,9 +10,17 @@ export class Email extends ValueObject {
     this._value = email;
   }
 
+  // Value Objects are immutable so properties are always readonly
   private readonly _value: string;
 
-  /* Type compatibility in TypeScript is based on structural subtyping. Private _value and a getter prevents that and makes types with the same structure incompatible. 
+  /* Type compatibility in TypeScript is based on structural subtyping.
+  For example, when assigning this Value Object to a type of other Value
+  Object that also has only "value" property and both of those are public
+  (their structures are equal), this means that TypeScript will treat those two different value objects as a same type (will let you assign
+  'Email' value object to a 'Country' value object, because both of them
+  only have 1 public 'value' property, so their structure is the same). 
+  Private '_value' and a getter prevents that behavior and makes types
+  with the same structure incompatible. 
   https://www.typescriptlang.org/docs/handbook/type-compatibility.html#private-and-protected-members-in-classes  */
   get value(): string {
     return this._value;
