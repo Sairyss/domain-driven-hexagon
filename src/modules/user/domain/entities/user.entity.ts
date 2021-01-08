b358c1a7e06c1135009fb3d137aca6cb767ff354
user <sairysss@gmail.com>
2021-01-08 19:05:42 +0100

    docs: added readonly description to user entity

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -17,24 +17,29 @@ export class UserEntity extends Entity<UserProps> {
   constructor(props: UserProps) {
     super(props);
     UserEntity.validate(props); // validating at construction
-    this._email = props.email;
+    this.email = props.email;
     this._address = props.address;
   }
 
-  private _email!: Email;
+  private _address: Address;
 
-  private _address!: Address;
-
-  // Private properties and getters without a setter protects entity invariant and prevents it from being in an invalid state. Use "update" method instead to restrict invalid operations
-  get email(): Email {
-    return this._email;
-  }
-
+  /* Private properties and getters without a setter protects entity
+  from outside modifications by using assignment, for example:
+  "user.email = someOtherEmail". This technique only allows
+  updating value by using a setter method (see updateAddress below) */
   get address(): Address {
     return this._address;
   }
 
-  // Update method only changes properties that we allow, in this case only address. This prevents from illegal actions, for example setting email from outside by doing something like user.email = otherEmail
+  /* email is 'readonly', this means it can't be updated
+  even by using a setter. Use 'readonly' properties if you know that
+  a property never should be updated */
+  readonly email: Email;
+
+  /* Update method only changes properties that we allow, in this
+   case only address. This prevents from illegal actions, 
+   for example setting email from outside by doing something
+   like user.email = otherEmail */
   updateAddress(props: UpdateUserAddressProps): void {
     this._address = new Address({ ...this._address, ...props });
   }
