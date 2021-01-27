8fef146d976211b8c9f941ca66f7ed5f585df0be
user <sairysss@gmail.com>
2021-01-27 17:41:14 +0100

    fix: enabled class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 import { initDomainEventHandlers } from '@modules/domain-event-handlers';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExceptionInterceptor } from './infrastructure/interceptors/exception.interceptor';
@@ -8,6 +9,8 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ExceptionInterceptor());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   initDomainEventHandlers();
 
