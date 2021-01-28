83cc1c7bad121e6f887847813b55678bceff077c
user <sairysss@gmail.com>
2021-01-28 16:25:58 +0100

    fix: convert-props-to-object

--- src/core/utils/convert-props-to-object.util.ts
+++ src/core/utils/convert-props-to-object.util.ts
@@ -1,32 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types */
 import { Entity } from '../base-classes/entity.base';
 import { ValueObject } from '../base-classes/value-object.base';
+
+function isEntity(obj: unknown): obj is Entity<unknown> {
+  /**
+   * 'instanceof Entity' causes error here.
+   * Probably creates some circular dependency. This is a workaround
+   */
+  return (
+    Object.prototype.hasOwnProperty.call(obj, 'toObject') &&
+    Object.prototype.hasOwnProperty.call(obj, 'id') &&
+    ValueObject.isValueObject((obj as Entity<unknown>).id)
+  );
+}
+
+function convertToRaw(item: any): any {
+  if (ValueObject.isValueObject(item)) {
+    return item.getRawProps();
+  }
+  if (isEntity(item)) {
+    return item.toObject();
+  }
+  return item;
+}
+
 /**
- * Converts props that usually consist of Value Objects to a plain object.
+ * Converts Entity/Value Objects props to a plain object.
  * Useful for testing and debugging.
  * @param props
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 export function convertPropsToObject(props: any): any {
   const propsCopy = { ...props };
+
   // eslint-disable-next-line guard-for-in
   for (const prop in propsCopy) {
     if (Array.isArray(propsCopy[prop])) {
       propsCopy[prop] = (propsCopy[prop] as Array<unknown>).map(item => {
-        if (ValueObject.isValueObject(item)) {
-          return item.getRawProps();
-        }
-        if (Entity.isEntity(item)) {
-          return item.toObject();
-        }
-        return item;
+        return convertToRaw(item);
       });
     }
-    if (ValueObject.isValueObject(propsCopy[prop])) {
-      propsCopy[prop] = propsCopy[prop].getRawProps();
-    }
-    if (Entity.isEntity(propsCopy[prop])) {
-      propsCopy[prop] = propsCopy[prop].toObject();
-    }
+    propsCopy[prop] = convertToRaw(propsCopy[prop]);
   }
 
   return propsCopy;
