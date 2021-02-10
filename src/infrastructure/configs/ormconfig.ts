1bd7be188953c325ce523adea6fb5bc38ed18327
user <sairysss@gmail.com>
2021-02-10 18:56:12 +0100

    refactor: config now uses .env

--- src/infrastructure/configs/ormconfig.ts
+++ src/infrastructure/configs/ormconfig.ts
@@ -2,13 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'user',
-  password: 'password',
-  database: 'test-db',
+  host: process.env.DB_HOST,
+  port: Number.parseInt(process.env.DB_PORT as string, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [],
   autoLoadEntities: true,
-  // synchronize: true,
   logging: ['error', 'migration', 'schema'],
 };
