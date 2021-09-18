1f4f3e5491195a5978f45e93cb48a3e3c1974be6
user <sairysss@gmail.com>
2021-09-18 21:17:37 +0200

    fix: FindUserRequest  properties are now optional

--- src/modules/user/queries/find-users/find-users.request.dto.ts
+++ src/modules/user/queries/find-users/find-users.request.dto.ts
@@ -1,20 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { MaxLength, IsString, IsAlphanumeric, Matches } from 'class-validator';
+import {
+  MaxLength,
+  IsString,
+  IsAlphanumeric,
+  Matches,
+  IsOptional,
+} from 'class-validator';
 import { FindUsers } from 'src/interface-adapters/interfaces/user/find-users.interface';
 
 export class FindUsersRequest implements FindUsers {
   @ApiProperty({ example: 'France', description: 'Country of residence' })
   @MaxLength(50)
+  @IsOptional()
   @IsString()
   @Matches(/^[a-zA-Z ]*$/)
   readonly country: string;
 
   @ApiProperty({ example: '28566', description: 'Postal code' })
+  @IsOptional()
   @MaxLength(10)
   @IsAlphanumeric()
   readonly postalCode: string;
 
   @ApiProperty({ example: 'Grande Rue', description: 'Street' })
+  @IsOptional()
   @MaxLength(50)
   @Matches(/^[a-zA-Z ]*$/)
   readonly street: string;
