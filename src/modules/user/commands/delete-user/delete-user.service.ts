2dd34a93a13a4298d28ebade73113c8f95a2fc7b
Timon Back <timon.back+git@gmail.com>
2021-09-27 17:17:43 +0200

    Fix non-injected UserRepository to DeleteUserService

--- src/modules/user/commands/delete-user/delete-user.service.ts
+++ src/modules/user/commands/delete-user/delete-user.service.ts
@@ -1,8 +1,11 @@
 import { UserRepositoryPort } from '@modules/user/database/user.repository.port';
+import { CommandHandler } from '@nestjs/cqrs';
+import { UserRepository } from '../../database/user.repository';
 import { DeleteUserCommand } from './delete-user.command';
 
+@CommandHandler(DeleteUserCommand)
 export class DeleteUserService {
-  constructor(private readonly userRepo: UserRepositoryPort) {}
+  constructor(private readonly userRepo: UserRepository) {}
 
   async execute(command: DeleteUserCommand): Promise<void> {
     const found = await this.userRepo.findOneByIdOrThrow(command.userId);
