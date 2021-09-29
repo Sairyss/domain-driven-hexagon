32cb1b2bb0b38599da39f1c33878093530eda77a
Timon Back <timon.back+git@gmail.com>
2021-09-29 10:41:55 +0200

    Fix di for DeleteUserService

--- src/modules/user/commands/delete-user/delete-user.service.ts
+++ src/modules/user/commands/delete-user/delete-user.service.ts
@@ -1,11 +1,15 @@
 import { UserRepositoryPort } from '@modules/user/database/user.repository.port';
+import { Inject } from '@nestjs/common';
 import { CommandHandler } from '@nestjs/cqrs';
 import { UserRepository } from '../../database/user.repository';
 import { DeleteUserCommand } from './delete-user.command';
 
 @CommandHandler(DeleteUserCommand)
 export class DeleteUserService {
-  constructor(private readonly userRepo: UserRepository) {}
+  constructor(
+    @Inject(UserRepository)
+    private readonly userRepo: UserRepositoryPort,
+  ) {}
 
   async execute(command: DeleteUserCommand): Promise<void> {
     const found = await this.userRepo.findOneByIdOrThrow(command.userId);
