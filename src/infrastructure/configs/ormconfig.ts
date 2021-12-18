819bc044688ea08c3d7e0d18862b96db71d00af4
user <sairysss@gmail.com>
2021-12-18 12:01:20 +0100

    refactor: using env-var to validate environmental variables

--- src/infrastructure/configs/ormconfig.ts
+++ src/infrastructure/configs/ormconfig.ts
@@ -1,15 +1,43 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { get } from 'env-var';
 
-config(); // Initializing dotenv
+// Initializing dotenv
+config();
+
+/**
+ * Making sure all environmental variables
+ * exist and are validated.
+ */
+class DatabaseConfig {
+  public static readonly DB_HOST: string = get('DB_HOST')
+    .required()
+    .asString();
+
+  public static readonly DB_NAME: string = get('DB_NAME')
+    .required()
+    .asString();
+
+  public static readonly DB_PORT: number = get('DB_PORT')
+    .required()
+    .asIntPositive();
+
+  public static readonly DB_USERNAME: string = get('DB_USERNAME')
+    .required()
+    .asString();
+
+  public static readonly DB_PASSWORD: string = get('DB_PASSWORD')
+    .required()
+    .asString();
+}
 
 export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number.parseInt(process.env.DB_PORT as string, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: DatabaseConfig.DB_HOST,
+  port: DatabaseConfig.DB_PORT,
+  username: DatabaseConfig.DB_USERNAME,
+  password: DatabaseConfig.DB_PASSWORD,
+  database: DatabaseConfig.DB_NAME,
   entities: [],
   autoLoadEntities: true,
   connectTimeoutMS: 2000,
