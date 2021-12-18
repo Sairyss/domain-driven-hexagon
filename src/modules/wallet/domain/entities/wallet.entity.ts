2243405690d0c9e9173f0bcaa381a32c04fc69a7
user <sairysss@gmail.com>
2021-12-18 11:30:49 +0100

    feat: added small example business logic to wallet

--- src/modules/wallet/domain/entities/wallet.entity.ts
+++ src/modules/wallet/domain/entities/wallet.entity.ts
@@ -1,6 +1,8 @@
 import { ArgumentOutOfRangeException } from '@libs/exceptions';
 import { AggregateRoot } from '@libs/ddd/domain/base-classes/aggregate-root.base';
 import { UUID } from '@libs/ddd/domain/value-objects/uuid.value-object';
+import { Result } from '@src/libs/ddd/domain/utils/result.util';
+import { WalletNotEnoughBalanceError } from '../../errors/wallet.errors';
 
 export interface CreateWalletProps {
   userId: UUID;
@@ -22,8 +24,23 @@ export class WalletEntity extends AggregateRoot<WalletProps> {
     return wallet;
   }
 
-  // TODO: example business logic
+  deposit(amount: number): void {
+    this.props.balance += amount;
+    WalletEntity.validate(this.props);
+  }
 
+  withdraw(amount: number): Result<null, WalletNotEnoughBalanceError> {
+    if (this.props.balance - amount < 0) {
+      return Result.err(new WalletNotEnoughBalanceError());
+    }
+    this.props.balance -= amount;
+    WalletEntity.validate(this.props);
+    return Result.ok(null);
+  }
+
+  /**
+   * Protects wallet invariant.
+   */
   static validate(props: WalletProps): void {
     if (props.balance < 0) {
       throw new ArgumentOutOfRangeException(
---
+++ src/modules/wallet/errors/wallet.errors.ts
@@ -0,0 +1,11 @@
+import { ExceptionBase } from '@src/libs/exceptions';
+
+export class WalletNotEnoughBalanceError extends ExceptionBase {
+  static readonly message: 'Wallet has not enough balance';
+
+  public readonly code = 'WALLET.NOT_ENOUGH_BALANCE';
+
+  constructor(metadata?: unknown) {
+    super(WalletNotEnoughBalanceError.message, metadata);
+  }
+}
