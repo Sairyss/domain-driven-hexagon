0f9b2f2486a9cdfbbdcb84b6e0af40565944d00f
user <sairysss@gmail.com>
2022-07-25 10:44:20 +0200

    docs: updated comment

--- src/libs/types/index.ts
+++ src/libs/types/index.ts
@@ -1,5 +1,8 @@
-// Consider creating a bunch of shared custom utility types for different situations.
-// alternatively can use a library like https://github.com/andnp/SimplyTyped
+/** Consider creating a bunch of shared custom utility
+ * types for different situations.
+ * Alternatively you can use a library like
+ * https://github.com/andnp/SimplyTyped
+ */
 export * from './deep-partial.type';
 export * from './non-function-properties.type';
 export * from './object-literal.type';
