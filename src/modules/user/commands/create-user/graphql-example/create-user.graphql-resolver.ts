a1e2384a3fc52eefddcb2f8b2f59531543c1fd42
user <sairysss@gmail.com>
2022-12-11 12:13:31 +0100

    fix: graphql resolver return type

--- src/modules/user/commands/create-user/graphql-example/create-user.graphql-resolver.ts
+++ src/modules/user/commands/create-user/graphql-example/create-user.graphql-resolver.ts
@@ -3,6 +3,9 @@ import { CommandBus } from '@nestjs/cqrs';
 import { CreateUserCommand } from '../create-user.command';
 import { CreateUserGqlRequestDto } from './dtos/create-user.gql-request.dto';
 import { IdGqlResponse } from './dtos/id.gql-response.dto';
+import { AggregateID } from '@src/libs/ddd';
+import { UserAlreadyExistsError } from '@src/modules/user/domain/user.errors';
+import { Result } from 'oxide.ts/dist';
 
 // If you are Using GraphQL you'll need a Resolver instead of a Controller
 @Resolver()
@@ -15,8 +18,9 @@ export class CreateUserGraphqlResolver {
   ): Promise<IdGqlResponse> {
     const command = new CreateUserCommand(input);
 
-    const id = await this.commandBus.execute(command);
+    const id: Result<AggregateID, UserAlreadyExistsError> =
+      await this.commandBus.execute(command);
 
-    return new IdGqlResponse(id);
+    return new IdGqlResponse(id.unwrap());
   }
 }
