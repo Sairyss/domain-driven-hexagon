979dd0ac10ab17f67c5bdf25e8772b87ad92fd34
user <sairysss@gmail.com>
2023-02-25 20:55:22 +0100

    docs: updated exception codes comment

--- src/libs/exceptions/exception.codes.ts
+++ src/libs/exceptions/exception.codes.ts
@@ -3,8 +3,9 @@
  * exception is a good practice, since when that exception
  * is transferred to another process `instanceof` check
  * cannot be performed anymore so a `code` string is used instead.
- * code enum types can be stored in a separate file so they
- * can be shared and reused on a receiving side
+ * code constants can be stored in a separate file so they
+ * can be shared and reused on a receiving side (code sharing is
+ * useful when developing fullstack apps or microservices)
  */
 export const ARGUMENT_INVALID = 'GENERIC.ARGUMENT_INVALID';
 export const ARGUMENT_OUT_OF_RANGE = 'GENERIC.ARGUMENT_OUT_OF_RANGE';
